e1899d8731d12bce7cc2733b41541c203d60617a
Nakul Rathod <142690696+nakul0405@users.noreply.github.com>
2025-07-06 00:33:15 +0530

    Update config.py

--- config.py
+++ config.py
@@ -5,3 +5,4 @@ load_dotenv()  # 👈 Must be called before os.getenv
 
 TELEGRAM_BOT_TOKEN = os.getenv("TELEGRAM_BOT_TOKEN")
 ADMIN_CHAT_ID = os.getenv("ADMIN_CHAT_ID")
+APP_URL = "https://pintrest-saved-updater-production.up.railway.app"  # 👈 your deployed domain
